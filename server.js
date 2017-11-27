431c48b8e00451c4faa7192811e393ced5d84764
tawnycat <tawnyeleanor@gmail.com>
2017-11-26 19:50:51 -0500

    server side js, handlebars finished

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Serves public folder
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
